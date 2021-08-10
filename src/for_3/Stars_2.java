bc9512436396bbcd46af979f55527377166855a3
Ryan-Nam <alfofh7060@gmail.com>
2021-08-10 22:48:05 +1000

    Add Stars_2

--- src/for_3/Stars_2.java
+++ src/for_3/Stars_2.java
@@ -21,8 +21,12 @@ public class Stars_2 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         sc.close();
-        for (int i = 1; i < n + 1; i++) {
-            for (int j =1; j<i+1; j++){
+
+        for (int i = 1; i <= n ; i++) {
+            for (int j =1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for (int k = 0; k < i; k++) {
                 System.out.print("*");
             }
             System.out.println();
