bbb06dae8f2ab7e4028a9d5a9dc3df4c4ce467e4
Ryan-Nam <alfofh7060@gmail.com>
2021-08-18 23:00:41 +1000

    Arimetic sequence question

---
+++ src/function_6/Arithmetic_Sequence.java
@@ -0,0 +1,14 @@
+package function_6;
+
+/*
+problem
+If each digit of a positive integer X forms an arithmetic sequence, the number is said to be a single number. An arithmetic sequence is a sequence in which the difference between two consecutive numbers is constant. Write a program that, given N, prints the number of single numbers greater than or equal to 1 and less than or equal to N.
+
+input
+The first line is given a natural number N less than or equal to 1,000.
+
+Print
+Print the number of single numbers greater than or equal to 1 and less than or equal to N in the first line.
+ */
+public class Arithmetic_Sequence {
+}
