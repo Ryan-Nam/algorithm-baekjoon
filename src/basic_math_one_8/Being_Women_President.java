c318cf5785c14423ce6e88da9a1fac1e0ce43bae
Ryan-Nam <alfofh7060@gmail.com>
2021-08-19 21:13:41 +1000

    Add Being women's president question

--- src/basic_math_one_8/Being_Women_President.java
+++ src/basic_math_one_8/Being_Women_President.java
@@ -1,4 +1,18 @@
 package basic_math_one_8;
+/*
+problem
+Joo-hee, who likes to attend neighborhood meetings, wants to take this opportunity to become the women's president, so she invites people from all floors to host the neighborhood.
 
+In order to live in this apartment, there is a condition, "If you want to live in room b on the a floor, you must bring as many people as the sum of the number of people from rooms 1 to b on the lower (a-1) floor". must be kept and entered.
+
+Assuming that there are no vacant houses in the apartment and all residents have been keeping the terms of this contract, print the number of people living in room n on floor k for the given positive integers k and n. However, apartments start from the 0th floor, and each floor has room 1 onwards, and i people live in room i on the 0th floor.
+
+input
+In the first line, the number of test cases T is given. And for each case, an integer k in the first line and an integer n in the second line are given as inputs.
+
+Print
+For each test case, print the number of residents in that house.
+ */
 public class Being_Women_President {
+
 }
