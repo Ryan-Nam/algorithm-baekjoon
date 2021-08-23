6b59f6346ef735f5a2d31b268f55363017f243c7
Ryan-Nam <alfofh7060@gmail.com>
2021-08-23 23:04:13 +1000

    Add String coration_alphabet question

---
+++ src/string_7/Croatian_Alphabet.java
@@ -0,0 +1,18 @@
+package string_7;
+/*
+Previously, it was not possible to input the Croatian alphabet in the operating system. Therefore, the Croatian alphabet was changed and entered as follows.
+
+For example, ljes=njak consists of six Croatian alphabets (lj, e, š, nj, a, k). Given a word, print how many Croatian alphabets it consists of.
+
+dž is unconditionally used as a single alphabet, and d and ž are not considered to be separate. The same goes for lj and nj. Alphabet not listed above is counted one letter at a time.
+
+input
+A maximum of 100 words is given on the first line. It consists only of lowercase letters and '-' and '='.
+
+Words are made up of the Croatian alphabet. The alphabets in the table of the problem description are entered in a modified form.
+
+Print
+Prints the number of Croatian alphabets in the given word as input.
+ */
+public class Croatian_Alphabet {
+}
