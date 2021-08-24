cda7d0aefb0eed63ef2b5e1c4c924978c00672ab
Ryan-Nam <alfofh7060@gmail.com>
2021-08-24 23:08:15 +1000

    Add sol

--- src/basic_math_one_8/Find_a_fountain.java
+++ src/basic_math_one_8/Find_a_fountain.java
@@ -1,5 +1,7 @@
 package basic_math_one_8;
 
+import java.util.Scanner;
+
 /*
 problem
 In an infinitely large array, the fractions are written as follows:
@@ -18,4 +20,29 @@ Print the fraction on the first line.
 
  */
 public class Find_a_fountain {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int X = in.nextInt();
+
+        int cross_count = 1, prev_count_sum = 0;
+
+        while (true) {
+
+            if (X <= prev_count_sum + cross_count) {
+
+                if (cross_count % 2 == 1) {
+                    System.out.print((cross_count - (X - prev_count_sum - 1)) + "/" + (X - prev_count_sum));
+                    break;
+                } else {
+                    System.out.print((X - prev_count_sum) + "/" + (cross_count - (X - prev_count_sum - 1)));
+                    break;
+                }
+
+            } else {
+                prev_count_sum += cross_count;
+                cross_count++;
+            }
+        }
+    }
+
 }
