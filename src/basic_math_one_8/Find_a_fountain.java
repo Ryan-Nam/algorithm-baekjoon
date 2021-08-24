016a3f6c8a0aa45e01dd9251b5db307d6366f471
Ryan-Nam <alfofh7060@gmail.com>
2021-08-24 22:56:49 +1000

    Add question

---
+++ src/basic_math_one_8/Find_a_fountain.java
@@ -0,0 +1,21 @@
+package basic_math_one_8;
+
+/*
+problem
+In an infinitely large array, the fractions are written as follows:
+
+
+The fractions listed in this way are 1/1 → 1/2 → 2/1 → 3/1 → 2/2 → … 1, 2, 3, 4, 5, ... Let's call it a fountain.
+
+Write a program to find the Xth fraction, given X.
+
+input
+The first line gives X(1 ≤ X ≤ 10,000,000).
+
+Print
+Print the fraction on the first line.
+
+
+ */
+public class Find_a_fountain {
+}
