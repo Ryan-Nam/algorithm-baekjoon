4546cb3f84be45a70e2d7c2adf540fffc126bab0
Ryan-Nam <alfofh7060@gmail.com>
2021-08-26 23:32:47 +1000

    Add problem

---
+++ src/basic_math_one_8/Honeycomb.java
@@ -0,0 +1,15 @@
+package basic_math_one_8;
+/*
+As shown in the picture above, there is a honeycomb made of hexagons. As shown in the figure, starting from room 1 in the center, the address can be assigned a number that increases by 1 in turns to neighboring rooms. Given a number N, write a program that counts how many rooms (including the beginning and the end) it passes through the minimum number of rooms from the center of the honeycomb to rooms 1 through N. For example, up to 13 goes through 3, and up to 58 goes through 5.
+
+input
+The first line gives N(1 ≤ N ≤ 1,000,000,000).
+
+Print
+Outputs how many rooms are passed when passing through the minimum number of rooms to the room given as input.
+
+picture can be shown in this link
+https://www.acmicpc.net/problem/2292
+ */
+public class Honeycomb {
+}
