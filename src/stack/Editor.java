3e75c2365731b629b9ae6a5aed370539e036ebcb
Ryan-Nam <alfofh7060@gmail.com>
2021-09-08 22:57:50 +1000

    Add stack Editor question

---
+++ src/stack/Editor.java
@@ -0,0 +1,22 @@
+package stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Editor {
+    public static void main(String[] args) {
+        // two stack between the cursor (line), left and right
+        Stack<String> left = new Stack<String>();
+        Stack<String> right = new Stack<String>();
+
+        Scanner sc = new Scanner(System.in);
+        //String ex)abcdefg
+        String str = sc.nextLine();
+        // how many time run
+        int n = sc.nextInt();
+        while (n-- > 0) { // repeat n times
+            
+        }
+
+    }
+}
